d001b7c65c0aa4e8f53b93244fccfb645ac8aca4
FRANCO1357 <fmelani13@gmail.com>
2022-09-15 18:12:12 +0200

    series card

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -1,7 +1,17 @@
 <main>
     <div class="jumbotron">
     </div>
-    <div class="series">
-        
+    <div class="container">
+        <div class="series">
+            <div class="row">
+                @foreach ($series as $serie)
+                    <div class="card">
+                        <img src="{{ $serie['thumb']}}" alt="">
+                        <p>{{ $serie['series']}}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
+    
 </main>
