feb91c52ccd607ed92e5830ff2e135a5004b465a
FRANCO1357 <fmelani13@gmail.com>
2022-09-15 17:47:46 +0200

    header & footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,62 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <nav>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </nav>        
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="button">
+                <a href="">SIGN UP NOW!</a>
+            </div>
+            <div class="social">
+                <h3>FOLLOW US</h3>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+                <a href="#"><i class="fa-solid fa-location-dot"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container">
+        <figure>
+            <img src="{{ asset('images/dc-logo.png')}}" alt="">
+        </figure>
+        <nav>
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+        <input type="text" placeholder="Search">
+    </div>
+</header>
+</template>
