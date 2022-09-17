04e7d8e069cd4a13bdcd969bfc3cced08043d85d
FRANCO1357 <fmelani13@gmail.com>
2022-09-17 17:36:27 +0200

    bottom info

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -33,6 +33,46 @@
             </div>
         </div>
     </div>
+    <div class="bottom-info">
+        <div class="container">
+            <div class="talent">
+                <h3>Talent</h3>
+                <div class="artists row">
+                    <h4>Art by:</h4>
+                    <div class="data">
+                        @foreach ($serie['artists'] as $artist)
+                        <a href="#">{{ $artist }}</a>
+                        @if( !$loop->last) , @else . @endif
+                        @endforeach
+                    </div>
+                </div>
+                <div class="writers row">
+                    <h4>Written by:</h4>
+                    <div class="data">
+                        @foreach ($serie['writers'] as $writer)
+                        <a href="#">{{ $writer }}</a>
+                        @if( !$loop->last) , @else . @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="specs">
+                <h3>Specs</h3>
+                <div class="series row">
+                    <h4>Series:</h4>
+                    <a href="">{{ $serie['series']}}</a>
+                </div>
+                <div class="price row">
+                    <h4>U.S. Price:</h4>
+                    <p>{{ $serie['price']}}</p>
+                </div>
+                <div class="sale-date row">
+                    <h4>On Sale Date:</h4>
+                    <p>{{ date('M d Y', strtotime($serie['sale_date'])) }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="buy-comics">
         <div class="container">
             <div class="card">
